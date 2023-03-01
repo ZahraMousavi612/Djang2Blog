0c2ee6cda6f9de2d18963d051d39874edd3fc3f4
ZahraMousavi612 <mousavizahra612@gamil.com>
2023-03-01 22:47:05 +0000

    deployment commit

--- env.py
+++ env.py
@@ -1,4 +1,5 @@
 import os
 
-os.environ["DATABASE_URL"]="postgres://rvyrktpd:25eLODs4XI2DQ_GOR2tKD89Ek5hY3oMG@trumpet.db.elephantsql.com/rvyrktpd"
-os.environ["SECRET_KEY"]="zahra@super_key"
+os.environ["DATABASE_URL"] = "postgres://rvyrktpd:25eLODs4XI2DQ_GOR2tKD89Ek5hY3oMG@trumpet.db.elephantsql.com/rvyrktpd"
+os.environ["SECRET_KEY"] = "zahra@super_key"
+os.environ["CLOUDINARY_URL"] = "cloudinary://237675737388532:bBKeagz3dIrZTwGnVaTbBBwuTI4@dc84nleun"
