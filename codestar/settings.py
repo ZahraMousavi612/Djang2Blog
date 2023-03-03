340f829b7e59af2730f13bbb9523739896923ad0
ZahraMousavi612 <mousavizahra612@gamil.com>
2023-03-03 10:08:05 +0000

    debuge code

--- codestar/settings.py
+++ codestar/settings.py
@@ -163,3 +163,19 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
